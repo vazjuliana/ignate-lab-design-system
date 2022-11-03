5512014cf1ecf313a1c38f5cabb6fee88bb614a5
Juliana Vaz da Silva <vazjuliana1993@gmail.com>
2022-11-03 19:25:48 -0300

    commit do workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  }, 
+  viteFinal: (config, {configType}) => {
+    if(configType === "PRODUCTION"){
+      config.base = '/ignate-lab-design-system/'
+    }
+
+    return config
   }
 }
